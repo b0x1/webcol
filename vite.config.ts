032ef50a023c56c58e218c369dd867e0ca4fdaaa
Boyang Xia <boyang@xia.at>
2026-04-07 19:04:34 +0200

    REvert vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,13 +20,13 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
   },
-  base: '/webcol/', // GitHub repo name as base URL.
+  base: "/webcol/",  // GitHub repo name as base URL.
   preview: { port: 4173 },
   server: { port: 4173 },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     sourcemap: false,
-    chunkSizeWarningLimit: 2500,
+    chunkSizeWarningLimit: 2500
   },
 });
